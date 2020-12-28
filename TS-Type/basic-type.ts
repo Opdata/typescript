6ff15559e2372b1dd48ebc6ff02c74591c01c978
Opdata <pb03641@gmail.com>
2020-12-28 09:47:36 +0900

    Basic Type Test

---
+++ TS-Type/basic-type.ts
@@ -0,0 +1,15 @@
+// JS 문자열 선언
+// const str = "hello";
+
+// TS 문자열 선언
+const str: string = "hello";
+
+// TS 숫자 선언
+let num: number = 123;
+
+// TS 숫자형 배열 선언
+const arr: Array<number> = [1, 2, 3];
+const item: number[] = [1, 2, 3];
+
+// TS 문자열 배열 선언
+const arr2: Array<string> = ["hello", "hi"];
