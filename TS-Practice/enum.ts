b85c687e09194fc1613c66038bff23266d9a93cd
Opdata <pb03641@gmail.com>
2021-01-04 19:26:26 +0900

    Numeric enum Test

---
+++ TS-Practice/enum.ts
@@ -0,0 +1,14 @@
+enum Shoes {
+  Nike,
+  Adidas,
+  Newbalance,
+}
+
+const myShoe = Shoes.Nike;
+console.log(myShoe); // 0
+
+const mySubShoes = Shoes.Adidas;
+console.log(mySubShoes); // 1
+
+const myThirdShoes = Shoes.Newbalance;
+console.log(myThirdShoes); // 2
