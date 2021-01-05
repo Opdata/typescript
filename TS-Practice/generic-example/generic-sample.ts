b7c02f586f8a170ac5c0227e806f1d863fd3c1de
Opdata <pb03641@gmail.com>
2021-01-05 22:57:20 +0900

    Interface Generic Example

--- TS-Practice/generic-example/generic-sample.ts
+++ TS-Practice/generic-example/generic-sample.ts
@@ -1,61 +1,102 @@
-/* Generic을 쓰지 않고 Union Type 사용 */
+// /* Generic을 쓰지 않고 Union Type 사용 */
 
-const emails: { value: string; selected: boolean }[] = [
-  { value: "naver.com", selected: true },
-  { value: "google.com", selected: false },
-  { value: "kakao.com", selected: false },
-];
+// const emails: { value: string; selected: boolean }[] = [
+//   { value: "naver.com", selected: true },
+//   { value: "google.com", selected: false },
+//   { value: "kakao.com", selected: false },
+// ];
 
-const products: { value: number; selected: boolean }[] = [
-  { value: 1, selected: true },
-  { value: 2, selected: false },
-  { value: 3, selected: false },
-];
+// const products: { value: number; selected: boolean }[] = [
+//   { value: 1, selected: true },
+//   { value: 2, selected: false },
+//   { value: 3, selected: false },
+// ];
 
-function CreateDropdown(item: { value: string | number; selected: boolean }) {
-  const option = document.createElement("option");
-  option.value = item.value.toString();
-  option.innerText = item.value.toString();
-  option.selected = item.selected;
-  return option;
-}
+// function CreateDropdown(item: { value: string | number; selected: boolean }) {
+//   const option = document.createElement("option");
+//   option.value = item.value.toString();
+//   option.innerText = item.value.toString();
+//   option.selected = item.selected;
+//   return option;
+// }
 
-emails.forEach((email) => {
-  const item = CreateDropdown(email);
-  const selectTag = document.querySelector("#email-dropdown");
-  selectTag.appendChild(item);
-});
+// emails.forEach((email) => {
+//   const item = CreateDropdown(email);
+//   const selectTag = document.querySelector("#email-dropdown");
+//   selectTag.appendChild(item);
+// });
 
-products.forEach((email) => {
-  const item = CreateDropdown(email);
-  const selectTag = document.querySelector("#email-dropdown");
-  selectTag.appendChild(item);
-});
+// products.forEach((email) => {
+//   const item = CreateDropdown(email);
+//   const selectTag = document.querySelector("#email-dropdown");
+//   selectTag.appendChild(item);
+// });
 
-/* Interface 사용 및 Union Type 사용 형태 */
-interface email {
-  value: string;
+// /* Interface 사용 및 Union Type 사용 형태 */
+// interface email {
+//   value: string;
+//   selected: boolean;
+// }
+
+// interface product {
+//   value: number; // 코드 중복
+//   selected: boolean; // 코드 중복
+// }
+
+// const useInterfaceemails: email[] = [
+//   { value: "naver.com", selected: true },
+//   { value: "google.com", selected: false },
+//   { value: "kakao.com", selected: false },
+// ];
+
+// const useInterfaceproducts: product[] = [
+//   { value: 1, selected: true },
+//   { value: 2, selected: false },
+//   { value: 3, selected: false },
+// ];
+
+// function useInterfaceCreateDropdown(item: email | product) {
+//   const option = document.createElement("option");
+//   option.value = item.value.toString();
+//   option.innerText = item.value.toString();
+//   option.selected = item.selected;
+//   return option;
+// }
+
+// useInterfaceemails.forEach((email) => {
+//   const item = useInterfaceCreateDropdown(email);
+//   const selectTag = document.querySelector("#email-dropdown");
+//   selectTag.appendChild(item);
+// });
+
+// useInterfaceproducts.forEach((email) => {
+//   const item = useInterfaceCreateDropdown(email);
+//   const selectTag = document.querySelector("#email-dropdown");
+//   selectTag.appendChild(item);
+// });
+
+/* Interface Generic 사용 */
+
+interface GenericItem<T> {
+  value: T;
   selected: boolean;
 }
 
-interface product {
-  value: number; // 코드 중복
-  selected: boolean; // 코드 중복
-}
-
-const useInterfaceemails: email[] = [
+const GenericEmail: GenericItem<string>[] = [
   { value: "naver.com", selected: true },
   { value: "google.com", selected: false },
   { value: "kakao.com", selected: false },
 ];
 
-const useInterfaceproducts: product[] = [
+const GenericProduct: GenericItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function useInterfaceCreateDropdown(item: email | product) {
+function GenericCreateDropdown(
+  item: GenericItem<string> | GenericItem<number>
+) {
   const option = document.createElement("option");
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -63,16 +104,14 @@ function useInterfaceCreateDropdown(item: email | product) {
   return option;
 }
 
-useInterfaceemails.forEach((email) => {
-  const item = useInterfaceCreateDropdown(email);
+GenericEmail.forEach((email) => {
+  const item = GenericCreateDropdown(email);
   const selectTag = document.querySelector("#email-dropdown");
   selectTag.appendChild(item);
 });
 
-useInterfaceproducts.forEach((email) => {
-  const item = useInterfaceCreateDropdown(email);
+GenericProduct.forEach((email) => {
+  const item = GenericCreateDropdown(email);
   const selectTag = document.querySelector("#email-dropdown");
   selectTag.appendChild(item);
 });
-
-/* Generic 사용 */
