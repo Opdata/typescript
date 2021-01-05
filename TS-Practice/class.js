6f3dff8344a73daf6562582fd8e24292f1bdb774
Opdata <pb03641@gmail.com>
2021-01-05 17:06:40 +0900

    JS this Test

---
+++ TS-Practice/class.js
@@ -0,0 +1,18 @@
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+const Ho = new Person("Ho", 29);
+console.log(Ho);
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const Jun = new Person("Jun", 22);
+
+console.log(Jun);
