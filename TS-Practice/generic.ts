7de03290d1b8438f2775ad85b6f4d8075cff65f6
Opdata <pb03641@gmail.com>
2021-01-05 22:48:31 +0900

    interface generic example

--- TS-Practice/generic.ts
+++ TS-Practice/generic.ts
@@ -5,8 +5,6 @@ function print<T>(text: T): T {
 
 print<string>("Hello");
 
-/*  */
-
 function ExamplePrint<T>(text: T): T {
   console.log(text);
   return text;
@@ -14,3 +12,18 @@ function ExamplePrint<T>(text: T): T {
 
 ExamplePrint<string>("aaaaabbbbbbcccc"); // String
 ExamplePrint<number>(100); // Number
+
+/* Interface Generic */
+// interface Dropdown {
+//   value: string;
+//   selected: boolean;
+// }
+
+// const DropdownObj: Dropdown = { value: 10, selected: true }; // 10 이 number 라서 Dropdown Interface의 string 타입인 value 에 오류발생
+
+interface Dropdown<T> {
+  value: T;
+  selected: boolean;
+}
+
+const GenericInterfaceObj: Dropdown<string> = { value: "hi", selected: false };
