6544d4f64f7e479dd256fe65839eec2e325fb1bd
Opdata <pb03641@gmail.com>
2021-01-05 14:47:51 +0900

    String Enum Test

--- TS-Practice/enum.ts
+++ TS-Practice/enum.ts
@@ -1,3 +1,4 @@
+/* Numeric Enum */
 enum Shoes {
   Nike,
   Adidas,
@@ -12,3 +13,15 @@ console.log(mySubShoes); // 1
 
 const myThirdShoes = Shoes.Newbalance;
 console.log(myThirdShoes); // 2
+
+/* String Enum */
+enum Cap {
+  Ball = "BallCap",
+  Hood = "HoodCap",
+}
+
+const myCap = Cap.Ball;
+console.log(myCap); // "BallCap"
+
+const myHood = Cap.Hood;
+console.log(Cap.Hood); //"Hood"
