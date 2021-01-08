f99bbdf54f6148a9af46a7c6b17ad712bce00441
Opdata <pb03641@gmail.com>
2021-01-08 17:07:50 +0900

    Comment Add

--- quiz/2_address-book/src/promise-generic.ts
+++ quiz/2_address-book/src/promise-generic.ts
@@ -3,6 +3,7 @@ function PromiseTest(): Promise<string[]> {
   const items: string[] = ["a", "b", "c"];
 
   return new Promise((resolve) => {
+    // 함수 정의 부분에서 반환타입을 정의해주었기 때문에 자동으로 resolve 에 대한 타입이 추론되어 추가적으로 정의해주지 않아도 된다.
     resolve(items);
   });
 }
