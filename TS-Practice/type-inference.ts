3982589ad86b2a9634c01dfe9b432322e343a098
Opdata <pb03641@gmail.com>
2021-01-09 14:57:13 +0900

    Type inference Test2

--- TS-Practice/type-inference.ts
+++ TS-Practice/type-inference.ts
@@ -1,3 +1,5 @@
+// Type inference Example 1 (variable, function parameter)
+
 let a; // any
 let b = 10; // number
 let c = "string"; // string
@@ -6,3 +8,34 @@ function getb(b = 10) {
   //parameter type number , return type number
   return b;
 }
+
+// Type inference Example 2 (interface, generic)
+
+interface Drop<T> {
+  value: T;
+  title: string;
+}
+
+const item: Drop<string> = {
+  value: "abc",
+  title: "hello",
+};
+
+// Type inference Example 3
+
+interface Dropdown<T> {
+  value: T;
+  title: string;
+}
+
+interface DetailDropDown<K> extends Dropdown<K> {
+  description: string;
+  tag: K;
+}
+
+const detailItem: DetailDropDown<string> = {
+  title: "abc",
+  description: "ab",
+  value: "hello",
+  tag: "inference",
+};
