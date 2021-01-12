55cfcc847282aec133ac3c22f44396f25d635d80
Opdata <pb03641@gmail.com>
2021-01-12 17:32:08 +0900

    Type Compatibility(Function) Test

--- TS-Practice/type-compatibility.ts
+++ TS-Practice/type-compatibility.ts
@@ -1,3 +1,4 @@
+/* interface type compatibility */
 interface Developer {
   name: string;
   skill: string;
@@ -12,3 +13,16 @@ let y: Person;
 
 x = y; // Error
 y = x; // OK
+
+/* function type compatibility */
+
+let add = function (a: number) {
+  // 내부 로직은 중요하지 않다.
+};
+
+let sum = function (a: number, b: number) {
+  // 구조적으로 add 보다 더 크다.
+};
+
+add = sum; // Error
+sum = add; // OK
