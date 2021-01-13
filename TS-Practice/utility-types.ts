ce3b54fcff46b656a9405e3eec54114b729cf96e
Opdata <pb03641@gmail.com>
2021-01-13 14:06:00 +0900

    utilty-types Partial implement

--- TS-Practice/utility-types.ts
+++ TS-Practice/utility-types.ts
@@ -39,3 +39,43 @@ const addressBook: Omit<AddressBook, "phone"> = {
 function updateProductItem(productItem: Partial<Product>) {
   //
 }
+
+// utility-types Partial 구현 방식1부터 개선해가며 방식4까지 Partial 순차적으로 구현
+interface UserProfile {
+  username: string;
+  email: string;
+  profilePhotoUrl: string;
+}
+
+// Optinal 을 사용한 구현 방식1
+// interface UserProfileUpdate {
+//   username?: string;
+//   email?: string;
+//   profilePhotoUrl?: string;
+// }
+
+// Optinal 사용과 직접 속성에 접근하여 구현 방식2
+// type UserProfileUpdate = {
+//   // UserProfileUpdate interface 와 동일한 구조
+//   username?: UserProfile["username"];
+//   email?: UserProfile["email"];
+//   profilePhotoUrl?: UserProfile["profilePhotoUrl"];
+// };
+
+// Optinal과 Mapped Type 반복을 활용한 구현 방식3( 2번 방식을 더 간결하게 표현한 방식)
+// type UserProfileUpdate = {
+//   [p in "username" | "email" | "profilePhotoUrl"]?: UserProfile[p];
+// };
+
+// keyof를 사용한 구현 방식4
+// type UserProfileUpdate = {
+//   // 방식3을 더 간결하게 표현한 방식
+//   [p in keyof UserProfile]?: UserProfile[p];
+// };
+
+// Generic 사용하여 더 간결하게
+type UserProfileUpdate<T> = {
+  // 방식4을 더 간결하게 표현한 방식,
+  // Mapped Type을 활용한 구현, 실제 Partial 구현 구조
+  [p in keyof T]?: T[p];
+};
