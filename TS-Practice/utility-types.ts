1b7c1015719bc612dd7174b4ef4e1b7b5d04cbfd
Opdata <pb03641@gmail.com>
2021-01-13 13:47:42 +0900

    utility-types Partial Example

--- TS-Practice/utility-types.ts
+++ TS-Practice/utility-types.ts
@@ -6,12 +6,14 @@ interface Product {
   stock: number;
 }
 
+// 상품 목록 받는 API 함수
 function fetch(): Promise<Product[]> {}
 
 /* utility-types Pick Example */
 
 // type ShoppingItem = Pick<Product, "id" | "name" | "price">; // preview 를 위한 코드
 
+// 상품 상세 정보 나타내는 함수
 function display(shoppingitem: Pick<Product, "id" | "name" | "price">) {}
 
 /* utility-types Omit Example */
@@ -30,3 +32,10 @@ const addressBook: Omit<AddressBook, "phone"> = {
   name: "이름",
   address: "관악",
 };
+
+// type updateProduct = Partial<Product>; // preview 를 위한 코드
+
+// 특정 상품 정보 업데이트 함수
+function updateProductItem(productItem: Partial<Product>) {
+  //
+}
