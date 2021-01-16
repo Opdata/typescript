c76752341c2edc73fa6e722a0f8bcfd50c2dff76
Opdata <pb03641@gmail.com>
2021-01-16 21:49:05 +0900

    Fetch Data Response Type Add(Atomize Types Ts File Add)

---
+++ project/src/covid/index.ts
@@ -0,0 +1,50 @@
+// types 관련 파일
+
+/* fetchCovidSummary Types */
+
+interface Country {
+  Country: string;
+  CountryCode: string;
+  Date: string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  Premium: any;
+  Slug: string;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
+
+interface Global {
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
+
+export interface CovidSummaryResponse {
+  Countries: Array<Country>;
+  Date: string;
+  Global: Global;
+  Message: string;
+}
+
+/* fetchCountryInfo Types */
+
+interface CountryInfo {
+  Country: string;
+  CountryCode: string;
+  Province: string;
+  City: string;
+  CityCode: string;
+  Lat: number;
+  Lon: number;
+  Cases: number;
+  Status: string;
+  Date: string;
+}
+
+export type CountryInfoResponse = Array<CountryInfo>;
